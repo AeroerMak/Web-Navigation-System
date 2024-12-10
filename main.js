eb4a381956b164f264c246fec9579b1211ef1c21
Aeroer Mak <58320066+AeroerMak@users.noreply.github.com>
2024-12-10 14:54:27 +0800

    Develop Website Navigation

---
+++ main.js
@@ -0,0 +1,23 @@
+// Header Content
+const headerContent = `
+    <header>
+        <nav>
+            <ul>
+                <li><a href="index.html">Home</a></li>
+                <li><a href="about.html">About</a></li>
+                <li><a href="contact.html">Contact</a></li>
+            </ul>
+        </nav>
+    </header>
+`;
+
+// Footer Content
+const footerContent = `
+    <footer>
+        <p>Copyright @2024, <a href='/'>Aeroer</a> All Rights Reserved</p>
+    </footer>
+`;
+
+// Dynamically load the header and footer
+document.getElementById("header").innerHTML = headerContent;
+document.getElementById("footer").innerHTML = footerContent;
